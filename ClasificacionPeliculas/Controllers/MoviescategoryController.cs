2efa922161f82f38c79d5f96ab2b8a9bbaed9a44
hermask8 <hermask8@gmail.com>
2023-03-01 23:02:22 -0600

    Editar

---
+++ ClasificacionPeliculas/Controllers/MoviescategoryController.cs
@@ -0,0 +1,125 @@
+﻿using ClasificacionPeliculas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClasificacionPeliculas.Controllers
+{
+    public class MoviescategoryController : Controller
+    {
+        private readonly MoviesContext _context;
+        public IActionResult Index()
+        {
+            MoviesContext _moviesContext = new MoviesContext();
+            IEnumerable<ClasificacionPeliculasModel.Moviescategory> moviescategories =
+                (from mc in _moviesContext.Moviescategories
+                 join m in _moviesContext.Movies on mc.MovieId equals m.Id
+                 join c in _moviesContext.Categories on mc.CategoryId equals c.Id
+                 select new ClasificacionPeliculasModel.Moviescategory
+                 {
+                     Id = mc.Id,
+                     MovieName = m.Title,
+                     CategoryName = c.Name
+                 }).ToList();
+            return View(moviescategories);
+        }
+
+        public IActionResult Create()
+        {
+            MoviesContext _moviesContext = new MoviesContext();
+            IEnumerable<ClasificacionPeliculasModel.Movie> movies = (from mc in _moviesContext.Movies
+                                                                     select new ClasificacionPeliculasModel.Movie
+                                                                     {
+                                                                         Id = mc.Id,
+                                                                         Title = mc.Title,
+                                                                     }).ToList();
+            IEnumerable<ClasificacionPeliculasModel.Category> categories = (from c in _moviesContext.Categories
+                                                                            select new ClasificacionPeliculasModel.Category
+                                                                            {
+                                                                                Id = c.Id,
+                                                                                Name = c.Name
+                                                                            }).ToList();
+            ClasificacionPeliculasModel.Moviescategory moviescategory = new ClasificacionPeliculasModel.Moviescategory();
+            moviescategory.Categories = categories.Select(s => new SelectListItem()
+            {
+                Value = s.Id.ToString(),
+                Text = s.Name
+            }).ToList();
+            moviescategory.Movies = movies.Select(s => new SelectListItem()
+            {
+                Value = s.Id.ToString(),
+                Text = s.Title
+            }).ToList();
+            return View(moviescategory);
+        }
+        [HttpPost]
+        public IActionResult Create (int MovieId,  int CategoryId)
+        {
+            Models.Moviescategory moviescategory = new Moviescategory
+            {
+                MovieId = MovieId,
+                CategoryId = CategoryId
+            };
+            MoviesContext _moviesContext = new MoviesContext();
+            _moviesContext.Moviescategories.Add(moviescategory);
+            _moviesContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            MoviesContext _moviesContext = new MoviesContext();
+            Models.Moviescategory moviescategory = _moviesContext.Moviescategories.FirstOrDefault(s => s.Id == id);
+            ClasificacionPeliculasModel.Moviescategory CPMmoviescategory = new ClasificacionPeliculasModel.Moviescategory
+            {
+                CategoryId = moviescategory.CategoryId,
+                MovieId = moviescategory.MovieId,
+                Id = moviescategory.Id
+            };
+
+            IEnumerable<ClasificacionPeliculasModel.Movie> movies = (from mc in _moviesContext.Movies
+                                                                     select new ClasificacionPeliculasModel.Movie
+                                                                     {
+                                                                         Id = mc.Id,
+                                                                         Title = mc.Title,
+                                                                     }).ToList();
+            IEnumerable<ClasificacionPeliculasModel.Category> categories = (from c in _moviesContext.Categories
+                                                                            select new ClasificacionPeliculasModel.Category
+                                                                            {
+                                                                                Id = c.Id,
+                                                                                Name = c.Name
+                                                                            }).ToList();
+
+            //ClasificacionPeliculasModel.Moviescategory moviescategory = new ClasificacionPeliculasModel.Moviescategory();
+            CPMmoviescategory.Categories = categories.Select(s => new SelectListItem()
+            {
+                Value = s.Id.ToString(),
+                Text = s.Name,
+                Selected = (s.Id == CPMmoviescategory.CategoryId)
+            }).ToList();
+            CPMmoviescategory.Movies = movies.Select(s => new SelectListItem()
+            {
+                Value = s.Id.ToString(),
+                Text = s.Title,
+                Selected = (s.Id == CPMmoviescategory.MovieId)
+            }).ToList();
+            return View(CPMmoviescategory);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Edit(int id, int MovieId, int CategoryId)
+        {
+            MoviesContext _moviesContext = new MoviesContext();
+            Models.Moviescategory moviescategory = _moviesContext.Moviescategories.FirstOrDefault(s => s.Id == id);
+            moviescategory.CategoryId = CategoryId;
+            moviescategory.MovieId = MovieId;
+            _moviesContext.Moviescategories.Update(moviescategory);
+            _moviesContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ClasificacionPeliculas/Controllers/PruebaController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClasificacionPeliculas.Models;
+
+namespace ClasificacionPeliculas.Controllers
+{
+    public class PruebaController : Controller
+    {
+        private readonly MoviesContext _context;
+
+        public PruebaController(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Prueba
+        public async Task<IActionResult> Index()
+        {
+            var moviesContext = _context.Moviescategories.Include(m => m.Category).Include(m => m.Movie);
+            return View(await moviesContext.ToListAsync());
+        }
+
+        // GET: Prueba/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Moviescategories == null)
+            {
+                return NotFound();
+            }
+
+            var moviescategory = await _context.Moviescategories
+                .Include(m => m.Category)
+                .Include(m => m.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (moviescategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(moviescategory);
+        }
+
+        // GET: Prueba/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id");
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id");
+            return View();
+        }
+
+        // POST: Prueba/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MovieId,CategoryId")] Moviescategory moviescategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(moviescategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", moviescategory.CategoryId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", moviescategory.MovieId);
+            return View(moviescategory);
+        }
+
+        // GET: Prueba/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Moviescategories == null)
+            {
+                return NotFound();
+            }
+
+            var moviescategory = await _context.Moviescategories.FindAsync(id);
+            if (moviescategory == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", moviescategory.CategoryId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", moviescategory.MovieId);
+            return View(moviescategory);
+        }
+
+        // POST: Prueba/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MovieId,CategoryId")] Moviescategory moviescategory)
+        {
+            if (id != moviescategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(moviescategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MoviescategoryExists(moviescategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", moviescategory.CategoryId);
+            ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Id", moviescategory.MovieId);
+            return View(moviescategory);
+        }
+
+        // GET: Prueba/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Moviescategories == null)
+            {
+                return NotFound();
+            }
+
+            var moviescategory = await _context.Moviescategories
+                .Include(m => m.Category)
+                .Include(m => m.Movie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (moviescategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(moviescategory);
+        }
+
+        // POST: Prueba/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Moviescategories == null)
+            {
+                return Problem("Entity set 'MoviesContext.Moviescategories'  is null.");
+            }
+            var moviescategory = await _context.Moviescategories.FindAsync(id);
+            if (moviescategory != null)
+            {
+                _context.Moviescategories.Remove(moviescategory);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MoviescategoryExists(int id)
+        {
+          return (_context.Moviescategories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
